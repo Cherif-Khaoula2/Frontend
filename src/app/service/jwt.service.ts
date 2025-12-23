a560c53fe92e6388a582bb223a38c62440af068c
Khaoula HAMADOUCHE <Khaoula.HAMADOUCHE@sarpi-dz.com>
2025-12-23 15:16:54 +0100

    first commit

--- src/app/service/jwt.service.ts
+++ src/app/service/jwt.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpResponse } from '@angular/common/http';
-import { catchError, map, Observable, tap } from 'rxjs';
+import { catchError, map, Observable } from 'rxjs';
 import { StorageService } from './storage-service/storage.service';
 import { Router } from '@angular/router';
 
@@ -11,36 +11,39 @@ const BASE_URL = "https://cmeapp.sarpi-dz.com/api/user/";
 })
 export class JwtService {
 
-  constructor(private http: HttpClient, private storage: StorageService, private router: Router) { }
+  constructor(
+    private http: HttpClient, 
+    private storage: StorageService, 
+    private router: Router
+  ) { }
 
   login(email: string, password: string): Observable<any> {
-    return this.http.post<HttpResponse<any>>(`${BASE_URL}authenticate`, { email, password }, { observe: 'response', withCredentials: true })
-      .pipe(
-        tap(() => this.logAuthentication('User Authentication')),
-        map((res: HttpResponse<any>) => {
-          const userData = res.body;
+    return this.http.post<HttpResponse<any>>(
+      `${BASE_URL}authenticate`, 
+      { email, password }, 
+      { observe: 'response', withCredentials: true }
+    ).pipe(
+      map((res: HttpResponse<any>) => {
+        const userData = res.body;
 
-          if (userData) {
-            this.storage.saveUser(userData);
+        if (userData) {
+          this.storage.saveUser(userData);
 
-            // Stocker les rôles et permissions dans le localStorage
-            const roles = userData.roles || [];
-            this.storage.saveRoles(roles);
+          // Stocker les rôles et permissions dans le localStorage
+          const roles = userData.roles || [];
+          this.storage.saveRoles(roles);
 
-            const permissions = userData.permissions || [];
-            this.storage.savePermissions(permissions);
-          } else {
-            console.error('Erreur: Données utilisateur non disponibles');
-          }
+          const permissions = userData.permissions || [];
+          this.storage.savePermissions(permissions);
+        }
 
-          return res;
-        }),
-        catchError((error) => {
-          console.error('Authentication failed', error);
-          this.router.navigate(['/login']);
-          throw error;
-        })
-      );
+        return res;
+      }),
+      catchError((error) => {
+        this.router.navigate(['/login']);
+        throw error;
+      })
+    );
   }
 
   logout(): void {
@@ -48,9 +51,5 @@ export class JwtService {
     this.http.get(`${BASE_URL}logout`, { withCredentials: true }).subscribe(() => {
       this.router.navigate(['/login']);
     });
-  }
-
-  logAuthentication(message: string): void {
-    console.log(message);
   }
 }
